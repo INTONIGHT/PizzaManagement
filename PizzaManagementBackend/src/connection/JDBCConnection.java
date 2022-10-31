f530742e166a8b69b7bd293cc61c4451e2f1c6c8
INTONIGHT <tyler.throop@gmail.com>
2022-10-31 16:33:38 -0700

    starting to build up the back end next to do is add the models then daos and then test everything to make sure it connects to the db then build out the front end

--- PizzaManagementBackend/src/connection/JDBCConnection.java
+++ PizzaManagementBackend/src/connection/JDBCConnection.java
@@ -1,11 +1,26 @@
 package connection;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
-public class JDBCConnection {
 
-	
+public class JDBCConnection {
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdc.Driver");
+			Properties props = new Properties();
+			InputStream input = JDBCConnection.class.getClassLoader()
+					.getResourceAsStream("connections.properties");
+			props.load(input);
+			String url = props.getProperty("url");
+			String username = props.getProperty("username");
+			String password = props.getProperty("password");
+			Connection conn = DriverManager.getConnection(url,username,password);
+			return conn;
+		} catch(SQLException | IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+			return null;
+	}
+
 }
--- PizzaManagementBackend/src/module-info.java
+++ PizzaManagementBackend/src/module-info.java
@@ -1,2 +1,3 @@
 module pizzaManagementBackend {
+	requires java.sql;
 }

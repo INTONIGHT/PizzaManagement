064be0656f4a4887f64e5fabb23cc33259347e4d
INTONIGHT <tyler.throop@gmail.com>
2022-10-31 16:21:07 -0700

    starting to build out the backend I have created the sql

---
+++ PizzaManagementBackend/src/connection/JDBCConnection.java
@@ -0,0 +1,11 @@
+package connection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+public class JDBCConnection {
+
+	
+}
---
+++ PizzaManagementBackend/src/module-info.java
@@ -0,0 +1,2 @@
+module pizzaManagementBackend {
+}

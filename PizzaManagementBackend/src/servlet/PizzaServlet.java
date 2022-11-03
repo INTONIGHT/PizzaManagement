668a3608fa80ee8f39229ee3068d92996cee8b6d
INTONIGHT <tyler.throop@gmail.com>
2022-11-03 10:56:58 -0700

    Starting to build out the rest API endpoints hopefully what I am doing is correct I am uncertain how to test at the moment

--- PizzaManagementBackend/src/servlet/PizzaServlet.java
+++ PizzaManagementBackend/src/servlet/PizzaServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,19 +14,14 @@ import com.google.gson.Gson;
 import daos.PizzaDAO;
 import daos.PizzaToppingsDAO;
 import daos.UserDAO;
+import models.Pizza;
+import models.PizzaToppings;
+import models.User;
 
 public class PizzaServlet extends HttpServlet{
-	class User{
+	class Login {
 		public String username;
 		public String password;
-	}
-	class PizzaToppings{
-		public int id;
-		public String toppingName;
-	}
-	class Pizza{
-		public int id;
-		public String pizzaName, pizzaToppings;
 	}
 	UserDAO udao = new UserDAO();
 	PizzaDAO pdao = new PizzaDAO();
@@ -48,5 +45,40 @@ public class PizzaServlet extends HttpServlet{
 				System.out.println("Default case");
 		}
 	}
-	
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		String uri = request.getRequestURI();
+		uri = uri.substring("/PizzaManagementBackend/PizzaServlet".length());
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Content-Type", "application/json");
+		session = request.getSession();
+		switch(uri) {
+		case "/login":
+			Login info = this.gson.fromJson(request.getReader(), Login.class);
+			User u = udao.Login(info.username, info.password);
+			session.setAttribute("login", u);
+			response.getWriter().append(gson.toJson(u));
+			break;
+		case "/getPizzas":
+			ArrayList<Pizza> pizzas = new ArrayList<>();
+			pizzas = pdao.getAllPizzas();
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.getWriter().append(gson.toJson(pizzas));
+			break;
+		case "/getPizza":
+			String pizzaName = this.gson.fromJson(request.getReader(), String.class);
+			Pizza pizza = pdao.getPizza(pizzaName);
+			response.getWriter().append(gson.toJson(pizza));
+			break;
+		case "createPizza":
+			User user = (User) session.getAttribute("login");
+			Pizza newPizza = new Pizza();
+			newPizza.setPizzaName(this.gson.fromJson(request.getReader(), String.class));
+			ArrayList<PizzaToppings> pizzaToppings = new ArrayList<>();
+			pizzaToppings.addAll((Collection<? extends PizzaToppings>) this.gson.fromJson(request.getReader(),PizzaToppings.class));
+			pdao.createPizza(newPizza.getPizzaName(), pizzaToppings);
+			response.getWriter().append(newPizza + "Has been created with: " + pizzaToppings);
+		}
+	}
 }
